c8fad35e7a23ea77f527fadb6955148b57d0284c
aritejhg <59247531+aritejhg@users.noreply.github.com>
2022-07-29 12:43:55 +0800

    deployment update

--- detect_openvino_1.py
+++ detect_openvino_1.py
@@ -1,11 +1,13 @@
 import os
 import time
 from functools import partial
-import cv2
+from xmlrpc.client import boolean
 import torch
 import openvino.runtime as ov
 
 from utils.detector_utils import DataStreamer, save_output, non_max_suppression, preprocess_image
+
+COORDS = [[231,340],[491,320],[751,344],[938,571],[644,847],[298,856],[23,597],[115,450],[231,350]]
 
 def load_model(model_xml_path: str, model_bin_path: str, target_device: str = "CPU"):
     # load IECore object
@@ -23,7 +25,7 @@ def load_model(model_xml_path: str, model_bin_path: str, target_device: str = "C
     return OVExec
 
 
-def inference(input_path: str, OVExec, output_dir: str, threshold: float, save: bool = False, debug: bool = False) -> None:
+def inference(input_path: str, OVExec, output_dir: str, threshold: float, save: bool = False, debug: bool = False) -> boolean:
     """Run Object Detection Application
     """
     if debug:
@@ -62,8 +64,9 @@ def inference(input_path: str, OVExec, output_dir: str, threshold: float, save: 
             print('Estimated Inference FPS: {} FPS Single Image'.format(fps))
 
         detections = non_max_suppression(
-            output_data, conf_thres=threshold, iou_thres=0.5)
-        print (detections)
+            output_data, conf_thres=threshold, iou_thres=0.6)
+        if debug:
+            print (detections)
         if save:
             save_path = os.path.join(
                 output_dir, f"{input_path.split('/')[-1]}") 
@@ -80,18 +83,25 @@ def inference(input_path: str, OVExec, output_dir: str, threshold: float, save: 
         print(f'Final Estimated FPS: {i / (elapse_time):.2f}')
 
     return detections[0].shape[0]>0
-    
-base_path = "C:/Users/arite/Desktop/KMBIC/Collins Aerospace final codes/499-people-v1/"
 
-OVExec = load_model(
-    model_xml_path= base_path + "overhead_person_detector/best_openvino_model/best.xml", 
-    model_bin_path= base_path + "overhead_person_detector/best_openvino_model/best.bin", 
-    target_device= "CPU")
-import fnmatch
-for input in fnmatch.filter(os.listdir("C:/Users/arite/Desktop/KMBIC/cropped_test_set/"), '*.jpg'):
-    inference(
-        input_path= f"C:/Users/arite/Desktop/KMBIC/cropped_test_set/{input}",
-        OVExec= OVExec,
-        output_dir= "C:/Users/arite/Desktop/KMBIC/cropped_test_set/" + "/test", 
-        threshold= 0.65,
-        debug= True, save = True)
+
+# testing 
+
+# base_path = "C:/Users/arite/Desktop/KMBIC/Collins Aerospace final codes/499-people-v1/"
+
+# OVExec = load_model(
+#     model_xml_path= base_path + "overhead_person_detector/vanilla_150ep_openvino/best.xml", 
+#     model_bin_path= base_path + "overhead_person_detector/vanilla_150ep_openvino/best.bin", 
+#     target_device= "CPU")
+# import fnmatch
+# # for input in fnmatch.filter(os.listdir("C:/Users/arite/Desktop/KMBIC/Collins Aerospace final codes/499-people-v1/mask_test"), '*.jpg'):
+# inference(
+#         input_path= "C:/Users/arite/Desktop/KMBIC/datasets/test_set_collins/og/image(normal)1.jpg",
+#         OVExec= OVExec,
+#         output_dir= "C:/Users/arite/Desktop/KMBIC/Collins Aerospace final codes/499-people-v1/", 
+#         threshold= 0.55,
+#         debug= True, save = True)
+
+# IMPLEMENT SAVE_PREV
+# implement timestamp 
+# documentation
